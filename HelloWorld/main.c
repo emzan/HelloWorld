72cdd8f2c800f1b85f14f1c1b3dceb4284afa734
Emilio Zanetti <zanettiemilio@hotmail.com>
2018-03-05 17:16:40 +0800

    changeSmallest

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, int *argv[])
 {
-	int x;
+	if (argc < 2)
+	{
+		puts("Please specify a file name.\n");
+		return (1);
+	}
+
+	printf("I will specify a file %s\n.", argv[1]);
+
 
 	return (0);
 }
+
+// good luck!
